c81d2959f08e57c9bec52c3fca2dc8e6fd984994
Will Moller <wmoller@uchicago.edu>
2024-11-19 18:20:15 -0500

    made formulas self contained

--- DataVisualizationStreamlit.py
+++ DataVisualizationStreamlit.py
@@ -9,8 +9,8 @@ st.subheader("Visualization")
 df_mast = pd.read_csv('s2_aam_raw_data.csv', index_col = 0) # master data set
 df_imp = df_mast.copy() # imported data set
 
-### Step 4: Identify variables to be visualized
 
+### Step 1: Identify variables to be visualized
 
 # Define variables that can be visualized
 var_dict_able_to_visualize = {
@@ -34,7 +34,8 @@ var_dict_able_to_visualize = {title: var for title, var in var_dict_able_to_visu
 selected_variables = st.multiselect("Choose variables you would like to visualize:", options=var_dict_able_to_visualize.keys())
 var_dict_selected = {title: var for title, var in var_dict_able_to_visualize.items() if title in selected_variables}
 
-### Step 5: Ask user how to display data
+
+### Step 2: Ask user how to display data
 if var_dict_selected:
 
     # By which ID?
@@ -50,11 +51,9 @@ if var_dict_selected:
     # Compare to master data set?
     compare_to_master = st.radio("Would you like to compare to master data set?", ("Yes", "No"))
 
-    # Define dataframes and labels for each dataset
     dataframes_both = [(df_mast, 'Master Data Set'), (df_imp, 'Imported Data Set')]
     dataframes_imported = [(df_imp, 'Imported Data Set')]
 
-    # Set the dataframes based on the user's choice
     if compare_to_master == "Yes":
         dataframes = dataframes_both
     else:
@@ -63,20 +62,12 @@ if var_dict_selected:
     # Choose colors?
     colors = st.radio("Would you like to choose the colors for the graph?", ("Yes", "No"), index=1)
 
-
 else:
     dataframes = []
     colors = "No"
 
 
-### Step 6: Group variables into graph types
-
-binned_category_vars = ['salary', 'entry_wage', 'perf_review', 'age']
-time_vars = ['entry_date', 'leave_date']
-
-
-
-##### Step: Ask user which colors they would like to use for the graph
+### Step 3: Ask user which colors they would like to use for the graph
 
 # Define functions to mix colors
 def hex_to_rgb(hex_color):
@@ -99,55 +90,93 @@ employee_color_master_default = "#ff7f0e"
 applicant_color_imported_default = "#2ca02c"
 employee_color_imported_default = "#d62728"
 
+
+colors_dict = {}
+
 if colors == "Yes":
-
     if compare_to_master == "Yes":
-
         if identify_by_app_or_emp == "Applicant":
-            applicant_color_master = st.color_picker("Choose a color for Applicants in Master Data Set", applicant_color_master_default)
-            applicant_color_imported = st.color_picker("Choose a color for Applicants in Imported Data Set", applicant_color_imported_default)
-            applicant_axis_color = mix_colors(applicant_color_master, applicant_color_imported) 
+            colors_dict["applicant_color_master"] = st.color_picker(
+                "Choose a color for Applicants in Master Data Set", applicant_color_master_default
+            )
+            colors_dict["applicant_color_imported"] = st.color_picker(
+                "Choose a color for Applicants in Imported Data Set", applicant_color_imported_default
+            )
+            colors_dict["applicant_axis_color"] = mix_colors(
+                colors_dict["applicant_color_master"], colors_dict["applicant_color_imported"]
+            )
 
-        if identify_by_app_or_emp == "Employee":
-            employee_color_master = st.color_picker("Choose a color for Employees in Master Data Set", employee_color_master_default)
-            employee_color_imported = st.color_picker("Choose a color for Employees in Imported Data Set", employee_color_imported_default)
-            employee_axis_color = mix_colors(employee_color_master, employee_color_imported)
+        elif identify_by_app_or_emp == "Employee":
+            colors_dict["employee_color_master"] = st.color_picker(
+                "Choose a color for Employees in Master Data Set", employee_color_master_default
+            )
+            colors_dict["employee_color_imported"] = st.color_picker(
+                "Choose a color for Employees in Imported Data Set", employee_color_imported_default
+            )
+            colors_dict["employee_axis_color"] = mix_colors(
+                colors_dict["employee_color_master"], colors_dict["employee_color_imported"]
+            )
 
-        if identify_by_app_or_emp == "Both":
-            applicant_color_master = st.color_picker("Choose a color for Applicants in Master Data Set", applicant_color_master_default)
-            employee_color_master = st.color_picker("Choose a color for Employees in Master Data Set", employee_color_master_default)
-            applicant_color_imported = st.color_picker("Choose a color for Applicants in Imported Data Set", applicant_color_imported_default)
-            employee_color_imported = st.color_picker("Choose a color for Employees in Imported Data Set", employee_color_imported_default)
-            applicant_axis_color = mix_colors(applicant_color_master, applicant_color_imported) 
-            employee_axis_color = mix_colors(employee_color_master, employee_color_imported)
+        elif identify_by_app_or_emp == "Both":
+            colors_dict["applicant_color_master"] = st.color_picker(
+                "Choose a color for Applicants in Master Data Set", applicant_color_master_default
+            )
+            colors_dict["employee_color_master"] = st.color_picker(
+                "Choose a color for Employees in Master Data Set", employee_color_master_default
+            )
+            colors_dict["applicant_color_imported"] = st.color_picker(
+                "Choose a color for Applicants in Imported Data Set", applicant_color_imported_default
+            )
+            colors_dict["employee_color_imported"] = st.color_picker(
+                "Choose a color for Employees in Imported Data Set", employee_color_imported_default
+            )
+            colors_dict["applicant_axis_color"] = mix_colors(
+                colors_dict["applicant_color_master"], colors_dict["applicant_color_imported"]
+            )
+            colors_dict["employee_axis_color"] = mix_colors(
+                colors_dict["employee_color_master"], colors_dict["employee_color_imported"]
+            )
 
-
-    if compare_to_master == "No":
-
+    elif compare_to_master == "No":
         if identify_by_app_or_emp == "Applicant":
-            applicant_color_imported = st.color_picker("Choose a color for Applicants in Imported Data Set", "#2ca02c")
-        if identify_by_app_or_emp == "Employee":
-            employee_color_imported = st.color_picker("Choose a color for Employees in Imported Data Set", "#d62728")
-        if identify_by_app_or_emp == "Both":
-            applicant_color_imported = st.color_picker("Choose a color for Applicants in Imported Data Set", "#2ca02c")
-            employee_color_imported = st.color_picker("Choose a color for Employees in Imported Data Set", "#d62728")
-            applicant_axis_color = applicant_color_imported
-            employee_axis_color = employee_color_imported
+            colors_dict["applicant_color_imported"] = st.color_picker(
+                "Choose a color for Applicants in Imported Data Set", "#2ca02c"
+            )
+        elif identify_by_app_or_emp == "Employee":
+            colors_dict["employee_color_imported"] = st.color_picker(
+                "Choose a color for Employees in Imported Data Set", "#d62728"
+            )
+        elif identify_by_app_or_emp == "Both":
+            colors_dict["applicant_color_imported"] = st.color_picker(
+                "Choose a color for Applicants in Imported Data Set", "#2ca02c"
+            )
+            colors_dict["employee_color_imported"] = st.color_picker(
+                "Choose a color for Employees in Imported Data Set", "#d62728"
+            )
+            colors_dict["applicant_axis_color"] = colors_dict["applicant_color_imported"]
+            colors_dict["employee_axis_color"] = colors_dict["employee_color_imported"]
 
 else:
-    applicant_color_imported = applicant_color_imported_default
-    employee_color_imported = employee_color_imported_default
-    applicant_color_master = applicant_color_master_default
-    employee_color_master = employee_color_master_default
-    applicant_axis_color = mix_colors(applicant_color_master, applicant_color_imported)
-    employee_axis_color = mix_colors(employee_color_master, employee_color_imported)
+    colors_dict["applicant_color_imported"] = applicant_color_imported_default
+    colors_dict["employee_color_imported"] = employee_color_imported_default
+    colors_dict["applicant_color_master"] = applicant_color_master_default
+    colors_dict["employee_color_master"] = employee_color_master_default
+    colors_dict["applicant_axis_color"] = mix_colors(
+        colors_dict["applicant_color_master"], colors_dict["applicant_color_imported"]
+    )
+    colors_dict["employee_axis_color"] = mix_colors(
+        colors_dict["employee_color_master"], colors_dict["employee_color_imported"]
+    )
 
 
 
-### Step 7: Define function that transforms time variables
+### Step 4: Group variables into graph types and transform
+
+binned_category_vars = ['salary', 'entry_wage', 'perf_review', 'age']
+time_vars = ['entry_date', 'leave_date']
 
 
-def time_vars_transform(time_series, existing_time_unit = None):
+def time_vars_transform(time_series, title, existing_time_unit = None):
 
     if existing_time_unit:
         time_unit = existing_time_unit
@@ -159,9 +188,8 @@ def time_vars_transform(time_series, existing_time_unit = None):
 
     return time_series_transformed, time_unit
 
-### Step 8: Define function that transforms binned variables
 
-def binned_vars_transform(binned_series, existing_bins = None, existing_labels = None): # binned df needs to have a column for master and imported data so that same bins are applied to both
+def binned_vars_transform(binned_series, title, dataframes, existing_bins = None, existing_labels = None): # binned df needs to have a column for master and imported data so that same bins are applied to both
     
     col = binned_series.name
 
@@ -176,79 +204,73 @@ def binned_vars_transform(binned_series, existing_bins = None, existing_labels =
         key=f'{col} num bins'
         )
 
-        values = set().union(*(df[col].dropna() for df, _ in dataframes))
+        values = set().union(*(df[col].dropna() for df, _ in dataframes)) # pull values from both dfs so that same binning is applied
 
         min_value = min(values)
         max_value = max(values)
 
-        # Define bins and labels
-        if title == 'Age':
-            bins = np.linspace(min_value, max_value, num=num_bins).astype(int)
-            labels = [f"{int(bins[i])} - {int(bins[i+1])}" for i in range(len(bins) - 1)]
-        else:
-            bins = np.linspace(min_value, max_value, num=num_bins)
 
+        bins = np.linspace(min_value, max_value, num=num_bins)
+
+        if col == 'age':
+            bins = bins.astype(int)
+            labels = [f"{int(bins[i])} - {int(bins[i+1])}" for i in range(len(bins) - 1)]
         if max_value >= 1000:
             labels = [f"{bins[i] / 1000:.0f}K - {bins[i + 1] / 1000:.0f}K" for i in range(len(bins) - 1)]
-            labels = pd.Categorical(labels, ordered=True, categories=labels)
-
         else:
             labels = [f"{bins[i]:.2f} - {bins[i + 1]:.2f}" for i in range(len(bins) - 1)]    
 
 
-
-    # Apply binning  
-      
     series_to_return = pd.cut(binned_series, bins=bins, labels=labels, include_lowest=False)
     
     return series_to_return, bins, labels
 
 
-### Step 9: Define a function that turns columns into counts by unique values
 
-def histogram_transform(col):
+### Step 5: Define a function that turns columns into counts by unique values
 
-    values = set().union(*(df[col].dropna() for df, _ in dataframes))
+def histogram_transform(col, dataframes, identify_by_app_or_emp):
+
+    values = set().union(*(df[col].dropna() for df, _ in dataframes)) # find the unique values across both data sets
 
     str_values = sorted(v for v in values if isinstance(v, str))
     str_values_no_num = [v for v in str_values if not any(char.isdigit() for char in v)]
     str_values_num = [v for v in str_values if any(char.isdigit() for char in v)]
     str_values_num = sorted(str_values_num, key=lambda x: float(''.join(filter(str.isdigit, x))))
     float_values = sorted(v for v in values if isinstance(v, float))
-    unique_values = str_values_no_num + str_values_num  + float_values
+    unique_values = str_values_no_num + str_values_num + float_values # sorted
 
-    # Initialize plot_data
-    plot_data = pd.DataFrame({col: unique_values})
-
-    # Remove duplicates and filter for applicants and employees for each dataframe
+    plot_data = pd.DataFrame(index=unique_values)
     for df, label in dataframes:
-        df.drop_duplicates(subset= id_list + [col], keep='first', inplace=True)
+
+        df_copy = df.copy()
+        df_copy.drop_duplicates(subset= id_list + [col], keep='first', inplace=True)
 
         if identify_by_app_or_emp == "Applicant":
-            df_applicants = df[df['applicantid'].notna()]
+            df_applicants = df_copy[df_copy['applicantid'].notna()]
             count_by_applicants = df_applicants[col].value_counts().sort_index()
             plot_data[f'Applicants {label}'] = count_by_applicants.reindex(unique_values, fill_value=0).values
 
         if identify_by_app_or_emp == "Employee":
-            df_employees = df[df['employeeid'].notna()]
+            df_employees = df_copy[df_copy['employeeid'].notna()]
             count_by_employees = df_employees[col].value_counts().sort_index()
             plot_data[f'Employees {label}'] = count_by_employees.reindex(unique_values, fill_value=0).values
 
         if identify_by_app_or_emp == "Both":
-            df_applicants = df[df['applicantid'].notna()]
+            df_applicants = df_copy[df_copy['applicantid'].notna()]
             count_by_applicants = df_applicants[col].value_counts().sort_index()
             plot_data[f'Applicants {label}'] = count_by_applicants.reindex(unique_values, fill_value=0).values
 
-            df_employees = df[df['employeeid'].notna()]
+            df_employees = df_copy[df_copy['employeeid'].notna()]
             count_by_employees = df_employees[col].value_counts().sort_index()
             plot_data[f'Employees {label}'] = count_by_employees.reindex(unique_values, fill_value=0).values
 
     return plot_data, unique_values
 
 
-### Step: Define plot functions
+### Step 6: Define plot functions
 
-def width_calculator(i, sign):
+def width_calculator(i, x, sign, identify_by_app_or_emp, compare_to_master, width):
     if identify_by_app_or_emp == "Both":
         bar_widths = x + (sign)*(i+1/2)*width
     else:
@@ -259,40 +281,40 @@ def width_calculator(i, sign):
             bar_widths = x
     return bar_widths
 
-
-# Define plot functions
-def plot_applicants(ax):
+def plot_applicants(ax, x, dataframes, plot_data, colors_dict, identify_by_app_or_emp, compare_to_master, width):
     for i, (_, label) in enumerate(dataframes):
-        bar_widths = width_calculator(i, 1)
-        ax.bar(bar_widths, plot_data[f'Applicants {label}'], width,
-                label=f'Applicants ({label})', color= applicant_color_master if label == 'Master Data Set' else applicant_color_imported)
-    ax.set_ylabel(f'Applicants Count', color=applicant_axis_color)
-    ax.tick_params(axis='y', labelcolor=applicant_axis_color)
+        bar_widths = width_calculator(i, x, 1, identify_by_app_or_emp, compare_to_master, width)
+        ax.bar(bar_widths, plot_data[f'Applicants {label}'],
+               width,
+                label=f'Applicants ({label})',
+                color = colors_dict['applicant_color_master'] if label == 'Master Data Set' else colors_dict['applicant_color_imported'])
+    ax.set_ylabel(f'Applicants Count', color = colors_dict['applicant_axis_color'])
+    ax.tick_params(axis='y', labelcolor = colors_dict['applicant_axis_color'])
 
-def plot_employees(ax):
+def plot_employees(ax, x, dataframes, plot_data, colors_dict, identify_by_app_or_emp, compare_to_master, width):
     for i, (_, label) in enumerate(dataframes):
-        bar_widths = width_calculator(i,-1)
-        ax.bar(bar_widths, plot_data[f'Employees {label}'], width,
-                label=f'Employees ({label})', color= employee_color_master if label == 'Master Data Set' else employee_color_imported)
-    ax.set_ylabel(f'Employees Count', color=employee_axis_color)
-    ax.tick_params(axis='y', labelcolor=employee_axis_color)
+        bar_widths = width_calculator(i, x, -1, identify_by_app_or_emp, compare_to_master, width)
+        ax.bar(bar_widths, plot_data[f'Employees {label}'],
+               width,
+                label=f'Employees ({label})',
+                color = colors_dict['employee_color_master'] if label == 'Master Data Set' else colors_dict['employee_color_imported'])
+    ax.set_ylabel(f'Employees Count', color = colors_dict['employee_axis_color'])
+    ax.tick_params(axis='y', labelcolor = colors_dict['employee_axis_color'])
 
 
-### Step: Plot
-
+### Step 6: Plot
 
 for title, col in var_dict_selected.items():
-    
 
     if col in time_vars:
-        df_imp[col], time_unit = time_vars_transform(df_imp[col])
-        df_mast[col], _ = time_vars_transform(df_mast[col], time_unit)
+        df_imp[col], time_unit = time_vars_transform(df_imp[col], title=title)
+        df_mast[col], _ = time_vars_transform(df_mast[col], title=title, existing_time_unit=time_unit)
 
     if col in binned_category_vars:
-        df_imp[col], bins, labels = binned_vars_transform(df_imp[col])
-        df_mast[col], _, _ = binned_vars_transform(df_mast[col], bins,labels)
+        df_imp[col], bins, labels = binned_vars_transform(df_imp[col], title=title, dataframes=dataframes)
+        df_mast[col], _, _ = binned_vars_transform(df_mast[col], title=title, dataframes=dataframes, existing_bins=bins, existing_labels=labels)
     
-    plot_data, unique_values = histogram_transform(col)
+    plot_data, unique_values = histogram_transform(col, dataframes, identify_by_app_or_emp)
 
     # Set up positions for bars
     x = np.arange(len(unique_values))
@@ -316,15 +338,15 @@ for title, col in var_dict_selected.items():
 
     # Plot based on `plot_type`
     if identify_by_app_or_emp == "Applicant":
-        plot_applicants(ax)
+        plot_applicants(ax, x, dataframes, plot_data, colors_dict, identify_by_app_or_emp, compare_to_master, width)
         graph_intro_title = 'Applicants Grouped by'
         handles, labels = ax.get_legend_handles_labels()
         if len(unique_values) > 10:
             ax.tick_params(axis='x', rotation=90)
 
     if identify_by_app_or_emp == "Employee":
-        plot_employees(ax)
-        graph_intro_title = 'Applicants Grouped by'
+        plot_employees(ax, x, dataframes, plot_data, colors_dict, identify_by_app_or_emp, compare_to_master, width)
+        graph_intro_title = 'Employees Grouped by'
         handles, labels = ax.get_legend_handles_labels()
         if len(unique_values) > 10:
             ax.tick_params(axis='x', rotation=90)
@@ -332,8 +354,8 @@ for title, col in var_dict_selected.items():
     if identify_by_app_or_emp == "Both":
         ax1 = ax
         ax2 = ax1.twinx()
-        plot_applicants(ax1)
-        plot_employees(ax2)
+        plot_applicants(ax1, x, dataframes, plot_data, colors_dict, identify_by_app_or_emp, compare_to_master, width)
+        plot_employees(ax2, x, dataframes, plot_data, colors_dict, identify_by_app_or_emp, compare_to_master, width)
         graph_intro_title = 'Applicants and Employees Grouped by'
         handles1, labels1 = ax1.get_legend_handles_labels()
         handles2, labels2 = ax2.get_legend_handles_labels()
